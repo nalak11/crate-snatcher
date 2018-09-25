807774efa786da22d00406071262024b46a03e90
nalak11 <nalak@live.com.au>
2018-09-25 15:53:47 +1000

    Converted to use python-taglib instead of eyeD3, also now only compatible with python 2.7 and lower

--- crate-snatcher.py
+++ crate-snatcher.py
@@ -1,8 +1,9 @@
-import chunk
 import sys
 import shutil
-import eyed3
+from taglib import tagopen, InvalidMedia, ValidationError
 import os
+from librosa import beat
+
 from os import path
 
 def main():
@@ -23,7 +24,7 @@ def main():
 
     if len(sys.argv) == 2:
         if sys.argv[1] == "-h":
-            print("usage: [source_directory] [comparison_directory] [backup_directory][OPTIONS]")
+            print("usage: [library_directory] [current_directory] [backup_directory][OPTIONS]")
             print("options: \n -b : copy to backup directory\n -h : show help")
             return
 
@@ -35,31 +36,37 @@ def main():
         if index == 1:
             continue
         if path.exists(arg) == 0 or path.isdir(arg) == 0:
-            print("error: argument " + str(index) + " " + arg + " is not a valid directory")
+            print("error: argument " + str(index) + " \"" + arg + "\" is not a valid directory")
+            exit(1)
 
     if len(sys.argv) == 5:
         option = sys.argv[4]
 
-    src_dir = sys.argv[1]
-    comp_dir = sys.argv[2]
-    bak_dir = sys.argv[3]
+    library_dir = sys.argv[1]
+    current_dir = sys.argv[2]
+    backup_dir = sys.argv[3]
 
-    print("src_dir: " + src_dir)
-    print("comp_dir: " + comp_dir)
-    print("bak_dir: " + bak_dir)
+    print("library_dir: " + library_dir)
+    print("current_dir: " + current_dir)
+    print("backup_dir: " + backup_dir)
 
     # begin directory analysis
 
-    for dirName, subdirList, fileList in os.walk(comp_dir):
+    for dirName, subdirList, fileList in os.walk(current_dir):
         print("Found directory: %s" % dirName)
-        tracklist = [Track(path.join(dirName, fname)) for fname in fileList]
-
+        for fname in fileList:
+            ext = path.splitext(fname)[1].lower()
+            if ext == (".mp3" or ".wav" or ".flac"):
+                print(path.join(dirName, fname))
+                Track(path.join(dirName, fname))
+            else:
+                continue
     return
 
 
 class Track:
 
-    """Track class represents a specific audio file, with easily accessible data"""
+    """Track class represents a specific audio file"""
 
     def __init__(self, fpath):
 
@@ -67,16 +74,10 @@ class Track:
         self.fext = path.splitext(fpath)[1].lower()[1:]
         self.fname = path.splitext(path.split(fpath)[1])[0]
 
-        if self.fext == "mp3":
-            self.info = get_mp3info(fpath)
-            if self.info:
-                print(self.info)
+        self.tag = tagopen(fpath, readonly=False)
 
-        elif self.fext == "wav":
-            self.tags = parse_wavtags(fpath)
-
-        elif self.fext == "flac":
-            self.tags = parse_flactags(fpath)
+        for k, info in self.tag.items():
+            print(k + ": ",  info)
 
 
 
@@ -84,17 +85,7 @@ class Track:
 
 
 def track_comp(fsrc_dir, fcomp_dir, comp_fname):
-    dup_path = "NOT FOUND"
-    print("finding mp3 %s" % comp_fname)
-
-    for dirName, subdirList, fileList in os.walk(fsrc_dir):
-            for src_fname in fileList:
-                comp_f = eyed3.load(path.join(fcomp_dir, comp_fname))
-                src_f = eyed3.load(path.join(dirName, src_fname))
-                # if comp_f.tag.artist == src_fname:
-
-
-    return dup_path
+    pass
 
 def get_mp3info(fpath):
     """Retrieves basic track information for the mp3 from ID3 tag
@@ -116,12 +107,6 @@ def get_mp3info(fpath):
         return 0
 
     return info
-
-def parse_flactags(fpath):
-    pass
-
-def parse_wavtags(fpath):
-    pass
 
 if __name__ == "__main__":
     main()
